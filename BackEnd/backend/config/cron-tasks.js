945b82214664a39c26991362dd3158fa438802ae
ArielFDev <arielferrera1992@gmail.com>
2023-08-30 14:10:56 -0400

    backup automatico

--- BackEnd/backend/config/cron-tasks.js
+++ BackEnd/backend/config/cron-tasks.js
@@ -6,7 +6,7 @@
 //         published_at_null: true,      // so we add another condition here to filter entries that have not been published
 //         publish_at_lt: new Date(),
 //       });
-  
+
 //       // update published_at of articles
 //       await Promise.all(draftOrganismoToPublish.map(organismo => {
 //         return strapi.api.organismo.services.organismo.update(
@@ -16,3 +16,47 @@
 //       }));
 //     },
 //   };
+
+const cron = require("node-cron");
+const { exec } = require("child_process");
+
+cron.schedule("54 13 * * *", async () => {
+  const date = new Date();
+  const dateString = `${date.getDate()}${
+    date.getMonth() + 1
+  }${date.getFullYear()}`;
+  const customBackupPath =
+    "F:\\Trabajo\\Visual Studio Proyects\\FullStack proyects\\SIGAE\\SIGAE\\BackEnd\\backend\\database"; // Cambia esta ruta
+  const filePath = `${customBackupPath}\\backup_SIGAE_${dateString}.sql`;
+
+  try {
+    await backupDatabase(filePath);
+  } catch (error) {
+    console.error("Error en el respaldo:", error);
+  }
+});
+
+async function backupDatabase(filePath) {
+  const pgDumpPath = "pg_dump"; // Assuming 'pg_dump' is in your PATH
+  const pgPassword = "America1992"; // Cambia esto con tu contraseña
+
+  // Configura la variable de entorno PGPASSWORD con la contraseña
+  process.env.PGPASSWORD = pgPassword;
+
+  const cmd = `"${pgDumpPath}" -U postgres -Fc SIGAE > "${filePath}"`;
+
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      // Restaura la variable de entorno PGPASSWORD a su valor original
+      delete process.env.PGPASSWORD;
+
+      if (error) {
+        reject(error.message);
+      } else if (stderr) {
+        reject(stderr);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
--- BackEnd/backend/src/api/traza/controllers/traza.js
+++ BackEnd/backend/src/api/traza/controllers/traza.js
@@ -8,30 +8,70 @@ const { exec } = require("child_process");
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::traza.traza", ({ strapi }) => ({
+  // async backup(ctx) {
+  //   let date=new Date()
+  //   date=date.getDate()+""+(date.getMonth()+1)+""+date.getFullYear()
+  //   const cmd = await ejecutar(
+  //     `PGPASSWORD='America1992' pg_dump -U postgres -Fc SIGAE > ../Database/backup_SIGAE_${date}.sql`
+  //   );
+  //   console.log("Usuario", cmd);
+  //   ctx.body = cmd;
+  // },
+
   async backup(ctx) {
-    let date=new Date()
-    date=date.getDate()+""+(date.getMonth()+1)+""+date.getFullYear()
-    const cmd = await ejecutar(
-      `PGPASSWORD='America1992' pg_dump -U postgres -Fc SIGAE > ../Database/backup_SIGAE_${date}.sql`
-    );
-    console.log("Usuario", cmd);
-    ctx.body = cmd;
+    const date = new Date();
+    const dateString = `${date.getDate()}${date.getMonth() + 1}${date.getFullYear()}`;
+    const customBackupPath = "F:\\Trabajo\\Visual Studio Proyects\\FullStack proyects\\SIGAE\\SIGAE\\BackEnd\\backend\\database"; // Cambia esta ruta
+    const filePath = `${customBackupPath}\\backup_SIGAE_${dateString}.sql`;
+
+    try {
+      await backupDatabase(filePath);
+      ctx.body = "Backup Completed";
+    } catch (error) {
+      console.error("Error en el respaldo:", error);
+      ctx.body = "Backup Failed";
+    }
   },
 }));
 
-function ejecutar(params) {
+// function ejecutar(params) {
+//   return new Promise((resolve, reject) => {
+//     exec(params, (error, stdout, stderr) => {
+//       if (error) {
+//         // console.log(`error: ${error.message}`);
+//         reject(error.message);
+//       }
+//       if (stderr) {
+//         // console.log(`stderr: ${stderr}`);
+//         reject(stderr);
+//       }
+//       // console.log(`stdout: ${stdout}`);
+//       resolve(stdout+"Backup Completed");
+//     });
+//   });
+// }
+
+async function backupDatabase(filePath) {
+  const pgDumpPath = "pg_dump"; // Assuming 'pg_dump' is in your PATH
+  const pgPassword = "America1992"; // Cambia esto con tu contraseña
+
+  // Configura la variable de entorno PGPASSWORD con la contraseña
+  process.env.PGPASSWORD = pgPassword;
+
+  const cmd = `"${pgDumpPath}" -U postgres -Fc SIGAE > "${filePath}"`;
+
   return new Promise((resolve, reject) => {
-    exec(params, (error, stdout, stderr) => {
+    exec(cmd, (error, stdout, stderr) => {
+      // Restaura la variable de entorno PGPASSWORD a su valor original
+      delete process.env.PGPASSWORD;
+
       if (error) {
-        // console.log(`error: ${error.message}`);
         reject(error.message);
-      }
-      if (stderr) {
-        // console.log(`stderr: ${stderr}`);
+      } else if (stderr) {
         reject(stderr);
+      } else {
+        resolve();
       }
-      // console.log(`stdout: ${stdout}`);
-      resolve(stdout+"Backup Completed");
     });
   });
 }
